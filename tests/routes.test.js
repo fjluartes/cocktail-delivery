d51e7d8cfa7b9a97124af6349d1d6ac2f15f64d4
Fred <fluartes@pickup.ph>
2022-05-13 13:17:19 +0800

    Edits tests, testValues

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -38,7 +38,12 @@ describe('getUser', () => {
       .set('Authorization', `Bearer ${userToken}`)
       .send()
       .then(res => {
+        const data = JSON.parse(res.text);
         expect(res.statusCode).toBe(200);
+        expect(data).toHaveProperty('firstName');
+        expect(data).toHaveProperty('lastName');
+        expect(data).toHaveProperty('email');
+        expect(data).toHaveProperty('orders');
       });
   });
 });
@@ -74,12 +79,30 @@ describe('getClient', () => {
       .set('Authorization', `Bearer ${clientToken}`)
       .send()
       .then(res => {
+        const data = JSON.parse(res.text);
         expect(res.statusCode).toBe(200);
+        expect(data).toHaveProperty('name');
+        expect(data).toHaveProperty('email');
+        expect(data).toHaveProperty('contactNo');
+        expect(data).toHaveProperty('stores');
       });
   });
 });
 
 // store
+describe('addStore', () => {
+  test('Adds new store', () => {
+    return request(app)
+      .post('/api/stores/add')
+      .set('Authorization', `Bearer ${clientToken}`)
+      .send(values.newStore)
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toEqual("true");
+      });
+  });
+});
+
 describe('getStoresByClient', () => {
   test('Display stores under a client', () => {
     return request(app)
@@ -99,14 +122,33 @@ describe('getByStoreId', () => {
       .set('Authorization', `Bearer ${clientToken}`)
       .send()
       .then(res => {
-        const storeData = JSON.parse(res.text);
-        console.log(storeData);
+        const data = JSON.parse(res.text);
         expect(res.statusCode).toBe(200);
+        expect(data).toHaveProperty('name');
+        expect(data).toHaveProperty('address');
+        expect(data).toHaveProperty('contactNo');
+        expect(data).toHaveProperty('clientId');
+        expect(data).toHaveProperty('menu');
+        expect(data).toHaveProperty('isActive');
+        expect(data).toHaveProperty('orders');
       });
   });
 });
 
 // orders
+describe('addOrder', () => {
+  test('Adds new order', () => {
+    return request(app)
+      .post('/api/orders/add')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send(values.newOrder)
+      .then(res => {
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toEqual("true");
+      });
+  });
+});
+
 describe('getByOrderId', () => {
   test('Display order details by id', () => {
     return request(app)
@@ -114,14 +156,62 @@ describe('getByOrderId', () => {
       .set('Authorization', `Bearer ${clientToken}`)
       .send()
       .then(res => {
-        const orderData = JSON.parse(res.text);
-        expect(orderData).toHaveProperty('userId');
+        const data = JSON.parse(res.text);
         expect(res.statusCode).toBe(200);
+        expect(data).toHaveProperty('userId');
+        expect(data).toHaveProperty('userName');
+        expect(data).toHaveProperty('address');
+        expect(data).toHaveProperty('contactNo');
+        expect(data).toHaveProperty('storeId');
+        expect(data).toHaveProperty('items');
+        expect(data).toHaveProperty('total');
+        expect(data).toHaveProperty('status');
       });
   });
 });
 
 // items
+describe('getByItemId', () => {
+  test('Display item details by itemId', () => {
+    return request(app)
+      .get(`/api/items/lookup?i=${values.itemId}`)
+      .set('Authorization', `Bearer ${clientToken}`)
+      .send()
+      .then(res => {
+        const data = JSON.parse(res.text);
+        expect(res.statusCode).toBe(200);
+        expect(data).toHaveProperty('drinks');
+      });
+  });
+});
+
+describe('getByQuery', () => {
+  test('Display items by query', () => {
+    return request(app)
+      .get(`/api/items/query?s=${values.itemQuery}`)
+      .set('Authorization', `Bearer ${clientToken}`)
+      .send()
+      .then(res => {
+        const data = JSON.parse(res.text);
+        expect(res.statusCode).toBe(200);
+        expect(data).toHaveProperty('drinks');
+      });
+  });
+});
+
+describe('getByFilter', () => {
+  test('Display items by filter', () => {
+    return request(app)
+      .get(`/api/items/filter?c=${values.itemFilter}`)
+      .set('Authorization', `Bearer ${clientToken}`)
+      .send()
+      .then(res => {
+        const data = JSON.parse(res.text);
+        expect(res.statusCode).toBe(200);
+        expect(data).toHaveProperty('drinks');
+      });
+  });
+});
 
 afterAll(() => {
   mongoose.connection.close();
--- tests/testValues.js
+++ tests/testValues.js
@@ -19,7 +19,7 @@ module.exports = {
     "email": "fjluartes@gmail.com",
     "password": "123456"
   },
-  storeId: "627b63078f6e1702df7bfe4f",
+  storeId: "627b5d254dd7345e4d76cbc6",
   newStore: {
     "name": "Guilt",
     "address": "Makati City",
@@ -33,4 +33,22 @@ module.exports = {
     ]
   },
   orderId: "627b61dd14d5d308aa62576a",
+  newOrder: {
+    "userName": "Fred Luartes",
+    "address": "Bel-Air, Makati City",
+    "contactNo": "09171234567",
+    "storeId": "627b5d254dd7345e4d76cbc6",
+    "items": [
+      {
+        "itemId": "11000",
+        "name": "Mojito",
+        "price": 329,
+        "quantity": 4
+      }
+    ],
+    "total": 1326
+  },
+  itemId: "11007",
+  itemFilter: "Cocktail",
+  itemQuery: "Vodka"
 };
